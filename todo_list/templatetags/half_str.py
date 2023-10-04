c96764f24e940fee11e60950421605ce7602ff8a
Давид Галенков <@itstep.loc>
2023-10-04 15:47:01 +0600

    home_36_updated

--- todo_list/templatetags/half_str.py
+++ todo_list/templatetags/half_str.py
@@ -32,3 +32,61 @@ class SplitStringNode(template.Node):
         return ''
 
 
+@register.filter
+def reverse_string(value):
+    return value[::-1]
+
+
+@register.filter
+def multiplier(value, arg=1):
+    try:
+        value = float(value)
+        arg = float(arg)
+        return arg * value
+    except ValueError:
+        return value
+
+
+@register.tag
+def repeat_string(parser, token):
+    try:
+        tag_name, value, times = token.split_contents()
+    except ValueError:
+        raise template.TemplateSyntaxError("Тег %r требует два аргумента: строку и количество повторений."
+                                           % token.contents.split()[0])
+    return RepeatStringNode(value, times)
+
+
+class RepeatStringNode(template.Node):
+    def __init__(self, value, times):
+        self.value = template.Variable(value)
+        self.times = int(times)
+
+    def render(self, context):
+        value = self.value.resolve(context)
+        return value * self.times
+
+
+@register.tag
+def greeting(parser, token):
+    try:
+        tag_name, hour = token.split_contents()
+    except ValueError:
+        raise template.TemplateSyntaxError("Теуг %r требует один аргумент: час"
+                                           % token.split_contents()[0])
+    return GreetingNode(hour)
+
+
+class GreetingNode(template.Node):
+    def __init__(self, hour):
+        self.hour = int(hour)
+
+    def render(self, context):
+        if 0 <= self.hour < 12:
+            return "Доброе утро"
+        elif 12 <= self.hour < 18:
+            return "Добрый день"
+        else:
+            return "Добрый вечер"
+
+
